940777fce9f7c67b50427600f586466591915c40
Fidel Quedet <fidelquedet10@gmail.com>
2022-08-07 13:39:26 +0000

    Setting Message Broker

---
+++ orders/tasks.py
@@ -0,0 +1,17 @@
+from celery import shared_task
+
+from django.core.mail import send_mail
+from orders.models import Order
+
+@shared_task
+def order_created(order_id):
+    """
+    Task to send e-mail notification when the order is successfully created.
+    """
+
+    order = Order.objects.get(id=order_id)
+    subject = f"Order nr. {order.id}"
+    message = f"Dear {order.first_name}, \n\nYou have successfully place an order. \nYour order ID is {order.id}"
+    mail_sent = send_mail(subject, message, 'admin@gmail.com', [order.email])
+
+    return mail_sent
--- orders/views.py
+++ orders/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
 from orders.models import OrderItem
+from .tasks import order_created
 
 # Create your views here.
 def order_create(request):
@@ -18,6 +19,7 @@ def order_create(request):
 
             #clear the cart
             cart.clear()
+            order_created.delay(order.id)
             return render(request, 'order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
