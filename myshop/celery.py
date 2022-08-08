39bd4c13ec7e7e90cf26a1ed02e2444c708a2cab
Fidel Quedet <fidelquedet10@gmail.com>
2022-08-08 10:48:57 +0000

    Adding Custom action to the administration site

--- myshop/celery.py
+++ myshop/celery.py
@@ -4,9 +4,7 @@ from celery import Celery
 # set the default Django celery module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myshop.settings')
 
-broker_url = 'amqp://guest:guest@127.0.0.1:5672/myvhost'
-
-app = Celery('myshop', broker=broker_url)
+app = Celery('myshop')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
