13a02f4e05c03e177669d7854ec62ca4cd61999a
Fidel Quedet <fidelquedet10@gmail.com>
2022-08-09 06:18:32 +0000

    Render Order to PDF file

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from orders.views import order_create
+from orders.views import admin_order_pdf, order_create, admin_order_detail
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('create/', order_create, name='order_create')
+    path('create/', order_create, name='order_create'),
+    path('admin/order/<int:order_id>/', admin_order_detail, name='admin_order_detail'),
+    path('admin/order/<int:order_id>/pdf/', admin_order_pdf, name='admin_order_pdf')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
+from django.contrib.admin.views.decorators import staff_member_required
+
+from django.conf import settings
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+
+import weasyprint
 
 from cart.cart import Cart
 from orders.forms import OrderCreateForm
-from orders.models import OrderItem
+from orders.models import Order, OrderItem
 from .tasks import order_created
 
 # Create your views here.
@@ -30,3 +37,25 @@ def order_create(request):
         form = OrderCreateForm()
 
     return render(request, 'order/create.html', {'form': form, 'cart': cart })
+
+
+@staff_member_required
+def admin_order_detail(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+
+    return render(request, 'admin/orders/order/detail.html', {
+        'order': order
+    })
+
+@staff_member_required
+def admin_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+    html = render_to_string('orders/order/pdf.html', {
+        'order': order
+    })
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f"filename=order_{order.id}.pdf"
+    weasyprint.HTML(string=html).write_pdf(response, stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+
+    return response
